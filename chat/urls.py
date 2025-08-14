222eea2884aed3d3bacbe42e6af49ee509cbba64
chintan <chintanbhutiya@gmail.com>
2025-08-14 22:05:32 +0530

    update1

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'chat'
+
 urlpatterns = [
-    path('chat/<str:room_name>/', views.room, name='room'),
+    path('room/<str:room_name>/', views.room, name='room'),
 ] 
